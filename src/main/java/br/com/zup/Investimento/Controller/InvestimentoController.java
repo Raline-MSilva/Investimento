d62e25532d9076c25bad604dd4abc73cb1712dc7
Raline Silva <ralinesilva1@gmail.com>
2021-11-04 14:00:00 -0300

    Cadastrar Investidor

--- src/main/java/br/com/zup/Investimento/Controller/InvestimentoController.java
+++ src/main/java/br/com/zup/Investimento/Controller/InvestimentoController.java
@@ -1,4 +1,23 @@
 package br.com.zup.Investimento.Controller;
 
+import br.com.zup.Investimento.Dto.InvestimentoDTO;
+import br.com.zup.Investimento.Service.InvestimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("/investimento")
 public class InvestimentoController {
+
+    @Autowired
+    private InvestimentoService investimentoService;
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarInvestidor(InvestimentoDTO investidor){
+        investimentoService.cadastrarInvestidor(investidor);
+    }
+
+
 }
--- src/main/java/br/com/zup/Investimento/Service/InvestimentoService.java
+++ src/main/java/br/com/zup/Investimento/Service/InvestimentoService.java
@@ -1,4 +1,16 @@
 package br.com.zup.Investimento.Service;
 
+import br.com.zup.Investimento.Dto.InvestimentoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class InvestimentoService {
+    private List<InvestimentoDTO> investidores = new ArrayList<>();
+
+    public void cadastrarInvestidor (InvestimentoDTO investidor){
+        investidores.add(investidor);
+    }
 }
