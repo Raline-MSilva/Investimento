4732efa9bd3bd25f110da6de6dab79634d9007b9
Raline Silva <ralinesilva1@gmail.com>
2021-11-07 11:16:09 -0300

    Criar tabela

---
+++ src/main/java/br/com/zup/investimento/simulacao/Investimento.java
@@ -0,0 +1,90 @@
+package br.com.zup.investimento.simulacao;
+
+import br.com.zup.investimento.Risco;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Investimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true) //unica simulação
+    private String email;
+    private String nome;
+    @Column(unique = true)
+    private String cpf;
+    @Column(name = "valor_investido")
+    private double valorInvestido;
+    @Column(name = "periodo_de_aplicacao_meses")
+    private int periodoDeAplicacaoMeses;
+    private Risco risco;
+
+    public Investimento() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public void setValorInvestido(double valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+}
