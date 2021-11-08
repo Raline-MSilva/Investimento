b1095cdbba753d746e6156d419d22e49376c1525
Raline Silva <ralinesilva1@gmail.com>
2021-11-08 09:53:38 -0300

    Criar repositorio

---
+++ src/main/java/br/com/zup/investimento/simulacao/SimulacaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.investimento.simulacao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SimulacaoRepository extends CrudRepository<Investimento,Integer> {
+
+}
